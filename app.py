536af38a0c57fafe2cdab72b1a1a8b1fbf0d875b
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2022-05-09 22:29:45 -0300

    Tirei o menu de notícias e preparei uma versão da page de  denúncia

--- app.py
+++ app.py
@@ -26,30 +26,18 @@ def showIndex():
     pageIndex = render_template('index.html')
     return pageIndex
 
+@app.route('/denuncie', methods=['GET', 'POST'])
+def showPageDenuncie():
+    pageDenuncie = render_template('pageDenuncie.html')
+    if request.method == 'GET':
+        return pageDenuncie
+    else:
+        idadevitmia = request.form['idadevitima'].strip()
+        grauescolarvitima = request.form['grauescolarvit'].strip()
+        return pageDenuncie
+
 @app.route('/graficos', methods=['GET'])
 def showPageGraficos():
     lista = conn.getQTDAlunos()
     pageGraficos = render_template('pageGraficos.html', casos=lista)
     return pageGraficos
-
-# # Página do HUB de notícias.
-# @app.route('/noticias', methods=['GET', 'POST'])
-# def showPageHub():
-#     tabela = conn.getPost()
-#     pageHub = render_template('pageNoticias.html', noticias=tabela)
-#     if 'userEmail' and 'userPwd' in session:
-#         return pageHub + '<script>window.document.querySelector(".loginButton1").style.display = "none"; window.document.querySelector(".loginButton2").style.display = "none";</script>'
-#     else:
-#         return pageHub
-        
-# # Página das notícias.
-# @app.route('/noticias/<post>', methods=['GET', 'POST'])
-# def showPage(post):
-#     try:
-#         pagePost = render_template(f'pagePosts/{post}')    
-#         if 'userEmail' and 'userPwd' in session:
-#             return pagePost + '<script>window.document.querySelector(".loginButton1").style.display = "none"; window.document.querySelector(".loginButton2").style.display = "none";</script>'
-#         else:
-#             return pagePost
-#     except:
-#         return showErrorPage(post)
