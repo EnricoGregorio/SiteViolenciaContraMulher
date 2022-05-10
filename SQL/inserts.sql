e9b9c0681a4b88c3b2082ff493a56429ea29e882
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2022-05-09 21:16:52 -0300

    Correções de icons e add a base de dados

---
+++ SQL/inserts.sql
@@ -0,0 +1,65 @@
+USE dbcamara;
+
+-- Natureza do fato.
+INSERT INTO NaturezasFato(natureza)
+VALUES("Violência psicológica (ameaça)");
+INSERT INTO NaturezasFato(natureza)
+VALUES("Lesão corporal");
+INSERT INTO NaturezasFato(natureza)
+VALUES("Perseguição");
+INSERT INTO NaturezasFato(natureza)
+VALUES("Injúria");
+INSERT INTO NaturezasFato(natureza)
+VALUES("Feminicídio");
+INSERT INTO NaturezasFato(natureza)
+VALUES("Não informado");
+
+-- Grau de Escolaridade.
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Superior completo");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Superior incompleto");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Ensino Médio completo");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Ensino Médio incompleto");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Ensino Fundamental completo");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Ensino Fundamental incompleto");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Lê e escreve");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Analfabeto");
+INSERT INTO GrausEscolaridade(grau)
+VALUES("Não informado");
+
+-- Bairros.
+INSERT INTO Bairros(bairro)
+VALUES("Promissão I");
+INSERT INTO Bairros(bairro)
+VALUES("Promissão II");
+INSERT INTO Bairros(bairro)
+VALUES("Promissão III");
+INSERT INTO Bairros(bairro)
+VALUES("Flamboyant");
+INSERT INTO Bairros(bairro)
+VALUES("Jaderlândia");
+INSERT INTO Bairros(bairro)
+VALUES("Uraim");
+INSERT INTO Bairros(bairro)
+VALUES("JK");
+INSERT INTO Bairros(bairro)
+VALUES("Não informado");
+
+-- Raças.
+INSERT INTO Racas(raca)
+VALUES("Amarelo");
+INSERT INTO Racas(raca)
+VALUES("Branco");
+INSERT INTO Racas(raca)
+VALUES("Negro");
+INSERT INTO Racas(raca)
+VALUES("Pardo");
+INSERT INTO Racas(raca)
+VALUES("Vermelho");
